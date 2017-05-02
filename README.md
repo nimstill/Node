60f46103cbd6cf2cba0dd655134c12c2c2745aae
nimstill <1679917335@qq.com>
2017-05-02 22:55:40 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+##Node与Express开发
--- hello.js
+++ hello.js
@@ -7,3 +7,26 @@ http.createServer(function(req, res) {
 
 console.log('press Ctrl-C to terminate...');
 
+
+
+var http = require('http'),
+    fs   = require('fs');
+
+function serveStaticFile(res, path, contentType, responseCode) {
+    if (!responseCode) responseCode = 200;
+    fs.readFile(__dirname + path, function(err, data) {
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('500 - Internal Error');
+        } else {
+            res.writeHead(responseCode,{'Content-Type': ContentType});
+            res.end(data);
+        }
+    });
+}
+
+
+http.createServer(function(req, res){
+    var path = req.url.replace(/\/?:\?.*)?$/, '').toLowerCase();
+    
+})
